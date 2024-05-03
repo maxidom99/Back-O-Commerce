665dcdb61b64db01a1fe26ad1f606ce0d08e08df
Maxi Dom <maxidom99@gmail.com>
2024-05-02 21:07:45 -0300

    fix: Automatizacion

--- auto/GitPull.sh
+++ auto/GitPull.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 clear
 
+echo " 😺 GITHUB (PULL) - O-COMMERCE 😺 "
+echo "==================================="
+echo "       1) Pullear O-Commerce"
+echo "       2) Pullear Backend"
+echo "       3) Pullear Frontend"
+echo ""
+read -p "---> " opc
+
+if [ $opc -eq 1 ]; then
 git pull origin main
+sleep 0.6
+cd ../o-commerce/FrontO-Commerce
+git pull origin main
+sleep 0.6
+elif [ $opc -eq 2 ]; then
+git pull origin main
+sleep 0.6
+elif [ $opc -eq 3 ]; then
+cd ../o-commerce/FrontO-Commerce
+git pull origin main
+sleep 0.6
+fi
 sleep 1.2
 clear
--- auto/GitPush.sh
+++ auto/GitPush.sh
@@ -2,11 +2,55 @@
 clear
 chmod u+x GitPush.sh 2>/dev/null
 
+echo " 😺 GITHUB (PUSH) - O-COMMERCE 😺 "
+echo "==================================="
+echo "       1) Pushear O-Commerce"
+echo "       2) Pushear Backend"
+echo "       3) Pushear Frontend"
+echo ""
+read -p "---> " opc
+
+if [ $opc -eq 1 ]; then
+#PUSH BACKEND
 git add .
 git status
 sleep 1.1
-read -p "¿Descripcion del Commit? " commit
-git commit -m "$commit"
+read -p "¿Descripcion del Commit (BACKEND)? " commitback
+git commit -m "$commitback"
 git push -u origin main
 sleep 1.1
 clear
+
+#PUSH FRONTEND
+cd ../o-commerce/FrontO-Commerce
+git add .
+git status
+sleep 1.1
+read -p "¿Descripcion del Commit (FRONTEND)? " commitfront
+git commit -m "$commitfront"
+git push -u origin main
+sleep 1.1
+clear
+
+elif [ $opc -eq 2 ]; then
+git add .
+git status
+sleep 1.1
+read -p "¿Descripcion del Commit (BACKEND)? " commitback
+git commit -m "$commitback"
+git push -u origin main
+sleep 1.1
+clear
+
+elif [ $opc -eq 3 ]; then
+cd ../o-commerce/FrontO-Commerce
+git add .
+git status
+sleep 1.1
+read -p "¿Descripcion del Commit (FRONTEND)? " commitfront
+git commit -m "$commitfront"
+git push -u origin main
+sleep 1.1
+clear
+
+fi
